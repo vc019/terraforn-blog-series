eb2daa50e6b14c5eec9e8cce33e56780f3540f16
vc019 <vipin.chadha@gmail.com>
2020-10-03 20:10:43 +0100

    Delete instance.tf

--- instance.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "cw-blog5-ec2" {
-  ami = lookup(var.cw_ami, "eu-west-2")
-  instance_type = var.cw_instance_type
-  key_name = aws_key_pair.my_blog_key.key_name
-  security_groups = ["cw-blog-5-sg"]
-}
